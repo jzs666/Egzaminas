d6c815f94e7840f6b9bf151ec7ea7387c722d442
jzs666 <juozas_adamonis@yahoo.com>
2017-12-21 12:38:17 +0200

    paskutinis paskutinis ir dar

--- PandoraJourney-app/src/main/java/lt/egzaminas/App.java
+++ PandoraJourney-app/src/main/java/lt/egzaminas/App.java
@@ -9,11 +9,31 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+@EnableSwagger2
 @SpringBootApplication
 public class App extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
+	@Bean public Docket swaggerDocket() {
+		return new Docket(DocumentationType.SWAGGER_2)
+		.apiInfo(apiInfo())
+		.select()
+		.apis(RequestHandlerSelectors.basePackage("it.akademija"))
+		.build();
+		}
+		private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+		.title("IT Akademija REST Documentation")
+		.version("0.0.1-SNAPSHOT")
+		.build();
+		}
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
